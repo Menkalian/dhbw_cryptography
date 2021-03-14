8740d33eb55e3427e024b7bf972136aa1741638c
Kilian Krampf <kilian@krampf.de>
2021-03-14 02:39:46 +0100

    Create and adapt SQL statements

--- src/main/resources/data/initData.sql
+++ src/main/resources/data/initData.sql
@@ -12,22 +12,4 @@ VALUES
     ('normal'),
     ('intruder');
 
--- Fill in predefined participants
-INSERT INTO participants
-    (name, type_id)
-VALUES
-    ('branch_hkg', 1),
-    ('branch_cpt', 1),
-    ('branch_sfo', 1),
-    ('branch_syd', 1),
-    ('branch_wuh', 1),
-    ('msa', 2);
-
--- Fill in predefined channels
-INSERT INTO channel
-    (name, participant_01, participant_02)
-VALUES
-    ('hkg_wuh', 1, 5),
-    ('hkg_cpt', 1, 2),
-    ('cpt_syd', 2, 4),
-    ('syd_sfo', 4, 3);
+COMMIT;
