b5dac23f8795d3e5512dc5d4589330e51e284019
Kilian Krampf <kilian@krampf.de>
2021-03-14 15:00:24 +0100

    Adapt SQL-Queries

--- src/main/resources/data/selectChannels.sql
+++ src/main/resources/data/selectChannels.sql
@@ -1,3 +1,5 @@
-SELECT name, participant_01, participant_02
-FROM channel
-ORDER BY name;
+SELECT c.name as channelname, p1.name as participant01, p2.name as participant02
+FROM channel c
+         JOIN participants p1 ON p1.id = c.participant_01
+         JOIN participants p2 ON p2.id = c.participant_02
+ORDER BY c.name;
